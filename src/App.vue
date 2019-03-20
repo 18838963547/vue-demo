111c68be18d8ab5a7cb46faea706a3c9a3be0ce2
Panda <280717844@qq.com>
2019-03-20 19:54:13 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <router-link to="/login">login</router-link>
+       <router-link to="/register">register</router-link>
+       <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        //组建中的data必须是function
+        return{
+            msg:'nihao'
+        }
+    }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
+
+
